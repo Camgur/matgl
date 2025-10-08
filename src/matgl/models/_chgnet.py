7862ff181702802b23368ee3f425f2fd9c5109ea
Cameron Alexander McCord Gurwell <gurwellc@mcmaster.ca>
2025-10-08 16:21:34 -0400

    Added tensor_handling to CHGNet class
    Allows fine control for when to allow preallocated tensors to resize if they are mismatched. Especially important for molecular dynamics, where the tensor sizes need dynamic resizing.
    
    Signed-off-by: Cameron Alexander McCord Gurwell <gurwellc@mcmaster.ca>

--- src/matgl/models/_chgnet.py
+++ src/matgl/models/_chgnet.py
@@ -93,6 +93,7 @@ class CHGNet(MatGLModel):
         num_site_targets: int = 1,
         task_type: Literal["regression", "classification"] = "regression",
         error_handling: bool = True,
+        tensor_handling: bool = True,
         **kwargs,
     ):
         """
@@ -179,6 +180,10 @@ class CHGNet(MatGLModel):
                 handling of line graph construction. When error handling is triggered
                 in very rare cases (<0.001%), a small amount (1e-6) of perturbation is
                 added to line graph cutoff to avoid inconsistent line graph.
+            tensor_handling (bool): whether to handle tensor reallocation due to mismatches
+                during tensor preallocation. The projected effect on compute efficiency is
+                small. The reallocation happens before graph creation and only triggers if
+                the buffer from the preallocation is not large enough (RuntimeError).
             **kwargs: additional keyword arguments.
         """
         super().__init__()
@@ -326,6 +331,7 @@ class CHGNet(MatGLModel):
         state_attr: torch.Tensor | None = None,
         l_g: dgl.DGLGraph | None = None,
         error_handling: bool = True,
+        tensor_handling: bool = True,
     ):
         """Forward pass of the model.
 
@@ -335,6 +341,8 @@ class CHGNet(MatGLModel):
             l_g (dgl.DGLGraph, optional): Line graph. Defaults to None and is computed internally.
             error_handling (bool, optional): Whether to allow numerical tolerance when an error occurs in
                 l_g construction. Defaults to True.
+            tensor_handling: whether to handle tensor reallocation due to mismatch in tensor preallocation.
+                Defaults to True.
 
         Returns:
             torch.Tensor: Model output.
@@ -358,7 +366,8 @@ class CHGNet(MatGLModel):
         # create bond graph (line graph) with necessary node and edge data
         if self.use_bond_graph:
             if l_g is None:
-                bond_graph = create_line_graph(g, self.three_body_cutoff, directed=True, error_handling=error_handling)
+                bond_graph = create_line_graph(g, self.three_body_cutoff, directed=True, 
+                                               error_handling=error_handling, tensor_handling=tensor_handling)
             else:
                 # need to ensure the line graph matches the graph
                 bond_graph = ensure_line_graph_compatibility(g, l_g, self.three_body_cutoff, directed=True)
@@ -429,6 +438,7 @@ class CHGNet(MatGLModel):
         state_feats: torch.Tensor | None = None,
         graph_converter: GraphConverter | None = None,
         error_handling: bool = True,
+        tensor_handling: bool = True,
     ):
         """Convenience method to directly predict property from structure.
 
@@ -438,6 +448,8 @@ class CHGNet(MatGLModel):
             graph_converter: Object that implements a get_graph_from_structure.
             error_handling (bool, optional): Whether to allow numerical tolerance when an error occurs in
                 l_g construction. Defaults to True.
+            tensor_handling: whether to handle tensor reallocation due to mismatch in tensor preallocation.
+                Defaults to True.
 
         Returns:
             output (torch.tensor): output property
@@ -452,4 +464,5 @@ class CHGNet(MatGLModel):
         graph.ndata["pos"] = graph.ndata["frac_coords"] @ lattice[0]
         if state_feats is None:
             state_feats = torch.tensor(state_feats_default)
-        return self(g=graph, state_attr=state_feats, error_handling=error_handling)
+        return self(g=graph, state_attr=state_feats, 
+                    error_handling=error_handling, tensor_handling=tensor_handling)
